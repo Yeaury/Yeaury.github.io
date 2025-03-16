35cd692f742a33cc5ca30bb3c9cfdf61df5b7668
Yeaury <3144550951@qq.com>
2025-03-16 11:21:51 +0800

    Site updated: 2025-03-16 11:21:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/16/hello-world/","2025/02/17/设计模式/","2025/03/02/java 容器/"];function toRandomPost(){
+var posts=["2025/02/16/hello-world/","2025/02/17/设计模式/","2025/02/17/java面试题/java 容器/","2025/02/17/java面试题/计算机网络/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
